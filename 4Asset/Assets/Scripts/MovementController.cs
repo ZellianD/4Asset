0b278766d10bba13271c4ac9157850e648c1871f
Zell <ZellianDettmer@gmail.com>
2025-02-16 17:53:02 -0500

    no more random movement

--- 4Asset/Assets/Scripts/MovementController.cs
+++ 4Asset/Assets/Scripts/MovementController.cs
@@ -1,45 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
     // Where is the vehicle
-    Vector3 objectPosition = new Vector3(0, 0, 0);
+    Vector3 objectPosition;
 
     // How fast it should move in units per second
     float speed = 4f;
 
     // Direction vehicle is facing, must be normalized
-    Vector3 direction = new Vector3(1, 0, 0);   // or Vector3.right
+    Vector3 direction;   // or Vector3.right
  
     // The delta in position for a single frame
-    Vector3 velocity = new Vector3(0, 0, 0);   // or Vector3.zero
-  
-    internal Vector3 Direction
-    {
-        get { return direction; } // Provide it if needed
-        set // Only set a normalized copy!
-        {
-            direction = value.normalized;
-        }
-    }
-   
+    Vector3 velocity;   // or Vector3.zero
 
     // Start is called before the first frame update
     void Start()
     {
-        Camera camObj = FindObjectOfType<Camera>();
-        float totalCamHeight = camObj.orthographicSize * 2f;
-        float totalCamWidth = totalCamHeight * camObj.aspect;
         objectPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         // Velocity is direction * speed * deltaTime
-        velocity = direction * speed * Time.deltaTime;
+        velocity = direction * speed * Time.fixedDeltaTime;
 
         // Add velocity to position 
         objectPosition += velocity;
@@ -70,12 +58,13 @@ public class MovementController : MonoBehaviour
 
    public void SetDirection(Vector2 input)
     {
-        if (input != null)
-        {
+        //if (input != null)
+        //{
             direction = input.normalized;
 
             // Set the vehicle’s rotation to match the direction
             transform.rotation = Quaternion.LookRotation(Vector3.back, direction); // for 2D rotation
-        }
+        //}
     }
+
 }
